9b3dfa190dd22f4ec393bfae822d579180bb8221
Trilowy <49493635+trilowy@users.noreply.github.com>
2024-04-19 19:25:28 +0200

    feat: add Javascript LSP

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -127,9 +127,7 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        tsserver = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
