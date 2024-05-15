5631cbc845763d6a36816162d4dd94e6f1b24fb5
Trilowy <49493635+trilowy@users.noreply.github.com>
2024-05-15 20:13:26 +0200

    fix: WSL config

--- lua/options.lua
+++ lua/options.lua
@@ -22,6 +22,22 @@ vim.opt.showmode = false
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
+-- If we are on WSL
+if vim.fn.has 'windows' == 1 and vim.fn.has 'linux' == 1 then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = false,
+  }
+end
+
 -- Enable break indent
 vim.opt.breakindent = true
 
