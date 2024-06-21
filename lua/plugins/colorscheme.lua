fa18ef3f80b0d584dc97c4804d5f256648bc3b3e
Trilowy <49493635+trilowy@users.noreply.github.com>
2024-06-21 23:48:26 +0200

    feat: more contrasted color theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,5 +14,16 @@ return {
       -- Configure highlights
       vim.cmd.hi 'Comment gui=none'
     end,
+    opts = {
+      colors = {
+        palette = {
+          -- See https://github.com/rebelot/kanagawa.nvim/blob/master/lua/kanagawa/colors.lua for other color override
+          sumiInk3 = '#0E0D18', -- Background
+          sumiInk4 = '#151521', -- Darker foreground (line numbers, fold column, non-text characters), float borders
+          sumiInk5 = '#222238', -- Current line
+          waveBlue1 = '#334765', -- Popup background, visual selection background
+        },
+      },
+    },
   },
 }
