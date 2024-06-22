1ea449305524304b8b75b51bb1b682c483938717
Trilowy <49493635+trilowy@users.noreply.github.com>
2024-06-22 11:46:53 +0200

    feat: todo highlight and search for Rust

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,4 +1,26 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      signs = false,
+      keywords = {
+        TODO = {
+          alt = { 'todo!' },
+        },
+      },
+      highlight = {
+        pattern = [[(KEYWORDS)]],
+        comments_only = false,
+        keyword = 'bg',
+      },
+      search = {
+        pattern = [[\b(KEYWORDS)\b]],
+      },
+    },
+  },
 }
