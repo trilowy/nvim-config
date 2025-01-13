6445ca36685e3a66557104ea65aa0ad4454f8baa
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-01-13 20:02:07 +0100

    feat: do not autoformat HTML (zmpl) files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -15,7 +15,12 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
+      local disable_filetypes = {
+        c = true,
+        cpp = true,
+        -- Disable autoformat for zmpl
+        html = true,
+      }
       return {
         timeout_ms = 500,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
