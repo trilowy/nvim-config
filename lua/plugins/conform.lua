26bc1f66a725764a8ce395b2da2f333b07af0a1f
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-01-20 22:36:43 +0100

    feat: Zig disable format imports

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,13 +1,13 @@
 -- Zig organize imports on save
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = { '*.zig', '*.zon' },
-  callback = function(ev)
-    vim.lsp.buf.code_action {
-      context = { only = { 'source.organizeImports' } },
-      apply = true,
-    }
-  end,
-})
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   pattern = { '*.zig', '*.zon' },
+--   callback = function(ev)
+--     vim.lsp.buf.code_action {
+--       context = { only = { 'source.organizeImports' } },
+--       apply = true,
+--     }
+--   end,
+-- })
 
 -- Autoformat
 return {
