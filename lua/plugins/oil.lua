d37e6e2e83ec85b105babf51392b42763da2e7c1
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-04-01 23:06:36 +0200

    feat: show hidden files in Oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,12 +1,15 @@
 -- File/dir tree edition
 return {
   'stevearc/oil.nvim',
-  opts = {},
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
 
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
