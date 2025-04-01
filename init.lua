1d92c2a1148364abda4be19041c3b8c968384600
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-04-01 10:47:14 +0200

    feat: Copilot chat

--- init.lua
+++ init.lua
@@ -332,7 +332,7 @@ require('lazy').setup {
                 separator = ' ',
               },
               -- LSP to ignore
-              ignore_lsp = { 'null-ls', 'crates.nvim' },
+              ignore_lsp = { 'null-ls', 'crates.nvim', 'copilot' },
             },
           },
         },
@@ -616,6 +616,17 @@ require('lazy').setup {
       'mfussenegger/nvim-jdtls',
       ft = 'java',
     },
+
+    -- Copilot
+    -- https://github.com/CopilotC-Nvim/CopilotChat.nvim
+    {
+      'CopilotC-Nvim/CopilotChat.nvim',
+      dependencies = {
+        { 'zbirenbaum/copilot.lua' },
+        { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+      },
+      opts = {},
+    },
   },
 }
 
@@ -863,3 +874,5 @@ vim.keymap.set(
   '<cmd>silent !~/software/kubectl/helm-vault -f ~/credentials/helm-vault-pass -i %<cr>',
   { desc = '[h]elm-[v]ault encrypt/decrypt' }
 )
+
+vim.keymap.set('n', '<leader>i', '<cmd>CopilotChatToggle<cr>', { desc = 'A[i] prompt chat' })
