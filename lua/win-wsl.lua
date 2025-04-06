4f83230467a33821b665e1e3eb44864a29272634
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-04-06 14:45:43 +0200

    fix: copy emoji in neovim

--- lua/win-wsl.lua
+++ lua/win-wsl.lua
@@ -1,8 +1,8 @@
 vim.g.clipboard = {
   name = 'WslClipboard',
   copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
+    ['+'] = { 'sh', '-c', 'iconv -f UTF-8 -t UTF-16LE | clip.exe' },
+    ['*'] = { 'sh', '-c', 'iconv -f UTF-8 -t UTF-16LE | clip.exe' },
   },
   paste = {
     ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::OutputEncoding = [System.Text.Encoding]::UTF8; [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
