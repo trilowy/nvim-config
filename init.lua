fde72b028395d83eeac58ef7c3ef37d3f7b06dc4
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-12-12 00:47:18 +0100

    feat: prettier completion hover colors

--- init.lua
+++ init.lua
@@ -164,7 +164,7 @@ require('lazy').setup {
             sumiInk4 = '#151521', -- Darker foreground (line numbers, fold column, non-text characters), float borders
             sumiInk5 = '#222238', -- Current line
             waveBlue1 = '#334765', -- Popup background, visual selection background
-            waveBlue2 = '#AD4D2D', -- Popup selection
+            waveBlue2 = '#54546D', -- Popup selection
             springGreen = '#42C492', -- String
 
             -- kanagawa-lotus
@@ -177,6 +177,16 @@ require('lazy').setup {
             lotusAqua = '#3DA57D', -- Type
             lotusInk1 = '#1E1E1E', -- Text
             lotusYellow2 = '#986801', -- Pipe operator
+          },
+          -- See https://github.com/rebelot/kanagawa.nvim/blob/master/lua/kanagawa/themes.lua
+          theme = {
+            all = {
+              ui = {
+                pmenu = {
+                  bg = '#0E0D18',
+                },
+              },
+            },
           },
         },
       },
