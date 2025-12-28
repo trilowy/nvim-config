02579f8bf5ac46385c2d4617c9c2a11d248b9fcf
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-12-28 17:11:03 +0100

    fix: html formatter install command

--- lsp/html.lua
+++ lsp/html.lua
@@ -1,6 +1,6 @@
 -- HTML
 -- https://github.com/hrsh7th/vscode-langservers-extracted
--- Can be installed with: npm i -g scode-langservers-extracted
+-- Can be installed with: npm i -g vscode-langservers-extracted
 return {
   cmd = { 'vscode-html-language-server', '--stdio' },
   filetypes = { 'html' },
